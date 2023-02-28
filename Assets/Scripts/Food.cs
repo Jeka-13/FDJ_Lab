1e082f3448c5e4118388a439e0d8bdb454b99571
Eugeniu Chetrar <chetrar.eugeniu@isa.utm.md>
2023-02-28 02:56:16 +0200

    Default snake

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public Collider2D gridArea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    public void RandomizePosition()
+    {
+        Bounds bounds = gridArea.bounds;
+
+        float x = Random.Range(bounds.min.x+2, bounds.max.x-2);
+        float y = Random.Range(bounds.min.y+2, bounds.max.y-2);
+
+        x = Mathf.Round(x);
+        y = Mathf.Round(y);
+
+        transform.position = new Vector2(x, y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        RandomizePosition();
+    }
+
+}
